2849b446f32619fb5d6cb453e1424c01230566e6
frncscsrcc <frncscsrcc@gmail.com>
2016-07-17 19:46:40 +0200

    Init

---
+++ index.js
@@ -0,0 +1,57 @@
+var debug = require('debug')('jsaturday::factory');
+
+// -----------------------------------------------------------
+// This is a draft!
+// -----------------------------------------------------------
+
+module.exports = function JSaturdayModule(settings, info){
+
+  // INFO CHECKS
+  // ...
+
+  // SETTINGS CHECKS
+  // ...
+
+  debug('info', 'Loading module ' + info.name + ' ...');
+
+  this.api = undefined;
+  this.modules = undefined;
+  this.settings = undefined;
+  this.info = info;
+
+  // REQUIREMENTS CHECKS
+  // ...
+
+  if(info.api)
+    try{
+      this.api = require(info.api);
+      // API CHECKS
+      // ...
+    }catch(err){
+      debug('warning', 'Api not loaded!')
+    }
+
+  if(info.lib)
+    try{
+      // LIBs CHECKS
+      // ...
+      this.lib = require(info.lib);
+    }catch(err){
+      debug('warning', 'Lib not loaded!')
+    }
+
+  if(info.settings)
+    try{
+      this.settings = require(info.settings);
+    }catch(err){
+      debug('warning', 'Settings not loaded!')
+    }
+
+  return {
+    name: this.info.name,
+    api: this.api,
+    modules: this.modules,
+    settings: this.settings
+  }
+
+}
