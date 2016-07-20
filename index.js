9ea008391ebe6d7370237c29edc8922a2091ea69
Francesco Sirocco <sirocco.francesco@gmail.com>
2016-07-20 13:39:39 +0200

    -

--- index.js
+++ index.js
@@ -4,54 +4,102 @@ var debug = require('debug')('jsaturday::factory');
 // This is a draft!
 // -----------------------------------------------------------
 
-module.exports = function JSaturdayModule(settings, info){
+module.exports = function(settings){
+  return new JSaturday(settings);
+};
 
-  // INFO CHECKS
-  // ...
+function JSaturday(){
+  var globalObject = {};
 
-  // SETTINGS CHECKS
-  // ...
+  this.get = function(moduleName){
+    var module = globalObject[moduleName];
+    if(module)
+      return module;
+    throw new Error('Module ' + moduleName + ' not loaded!');  
+  };
 
-  debug('info', 'Loading module ' + info.name + ' ...');
+  this.testAdd = function(test){
+    globalObject[test] = 1;
+  };
 
-  this.api = undefined;
-  this.modules = undefined;
-  this.settings = undefined;
-  this.info = info;
+  this.loadModule = function(info, options){
 
-  // REQUIREMENTS CHECKS
-  // ...
+    debug('info', 'Loading module ' + info.name + ' ...');
 
-  if(info.api)
-    try{
-      this.api = require(info.api);
-      // API CHECKS
-      // ...
-    }catch(err){
-      debug('warning', 'Api not loaded!')
+    if(!info.name)
+      throw new Error('Missing info.name');
+
+    if(globalObject[info.name])
+      throw new Error('Module already loaded');
+
+    if(typeof info.dependsOn === 'string')
+      info.dependsOn = [info.dependsOn];
+
+    if(!info.dependsOn)
+      info.dependsOn = [];
+
+    // INFO CHECKS
+    for(var i = 0; i < info.dependsOn.length; i++){
+      if(!globalObject[ info.dependsOn[i] ]){
+        debug('error',  info.node + ' Missing dependsOn ' + info.dependsOn[i]);
+        throw new Error('Missing dependsOn ' + info.dependsOn[i]);
+      }
     }
 
-  if(info.lib)
-    try{
-      // LIBs CHECKS
-      // ...
-      this.lib = require(info.lib);
-    }catch(err){
-      debug('warning', 'Lib not loaded!')
-    }
+    var api;
+    var modules;
+    var settings;
+    var client;
 
-  if(info.settings)
-    try{
-      this.settings = require(info.settings);
-    }catch(err){
-      debug('warning', 'Settings not loaded!')
-    }
+    if(info.api)
+      try{
+        api = require(info.api)(this);
+        // API CHECKS
+        // ...
+      }catch(err){
+        debug('warning', 'Api not loaded!');
+      }
 
-  return {
-    name: this.info.name,
-    api: this.api,
-    lib: this.lib,
-    settings: this.settings
-  }
+    if(info.lib)
+      try{
+        // LIBs CHECKS
+        // ...
+        lib = require(info.lib)(this);
+      }catch(err){
+        debug('warning', 'Lib not loaded!');
+      }
+
+    if(info.settings)
+      try{
+        settings = require(info.settings)(this);
+      }catch(err){
+        debug('warning', 'Settings not loaded!');
+      }
+
+    if(info.client)
+      try{
+        client = require(info.client)(this);
+      }catch(err){
+        debug('warning', 'Client not loaded!');
+      }
+
+    var commonInterface = {
+      getModule: this.get,
+      name: info.name,
+      api: api,
+      client: client,
+      lib: lib,
+      settings: settings
+    };
+
+    // Add module to globalObject
+    globalObject[info.name] = commonInterface;
+
+    debug('info', 'Loaded module ' + info.name + '!\n');
+
+    return true;
+
+  };
 
 }
+
